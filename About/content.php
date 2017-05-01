84fe2ee9bb8e788fff2827faf6f50eddefe87a14
Madison Aster <info@MadisonAster.com>
2017-04-30 23:18:45 -0700

    pdf testing2

--- About/content.php
+++ About/content.php
@@ -3,6 +3,8 @@ $content = file("./About/content.html");
 foreach ($content as &$value) {
     if(strpos($value,"{xpTable}") !== false){
         include 'xpTable.php';
+    }elseif(strpos($value,"{ResumeTemplate}") !== false){
+        include 'ResumeTemplate.html';
     }else{
         echo $value;
     };
