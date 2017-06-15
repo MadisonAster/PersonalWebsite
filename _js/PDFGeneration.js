484edbba370d0de7b5319d898e44cfabd2bfe482
Madison Aster <info@MadisonAster.com>
2017-06-14 22:51:04 -0700

    cv testing39

--- _js/PDFGeneration.js
+++ _js/PDFGeneration.js
@@ -480,9 +480,9 @@ function CVContactDetails(doc, PositionTitle) {
 
     doc.setFontSize(20);
     doc.setTextColor(0, 0, 0);
-    doc.setCharSpace(3);
+    doc.setCharSpace(0);
     //doc.setFontStyle('bold');
-    //doc.text(200, 30, '.thomas mcvay');
+    doc.text(200, 120, '.thomas mcvay');
     doc.drawText(200, 30, ['nanana', {text: '.thomas mcvay', fontSize: 20, TextColor: [255, 0, 0], charSpace: 0, font: 'CandaraLL'},'hahaha']);
 
     doc.setFontSize(18);
