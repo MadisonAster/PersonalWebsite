657b16f89eb4f326c3fe2b31bd98c6ec6d4ca255
Madison Aster <info@MadisonAster.com>
2017-06-14 23:09:15 -0700

    cv testing45

--- _js/PDFGeneration.js
+++ _js/PDFGeneration.js
@@ -465,8 +465,8 @@ function CVContactDetails(doc, PositionTitle) {
     */
     
     doc.addFont('msgothic.ttf', 'MsGothic', 'normal', 'Identity-H');
-    doc.addFont('Candara.ttf', 'CandaraLL', 'normal', 'WinAnsiEncoding');
-    doc.addFont('Candarai.ttf', 'CandaraLL', 'italic', 'WinAnsiEncoding');
+    doc.addFont('Candara.ttf', 'CandaraLL', 'normal', 'StandardEncoding');
+    doc.addFont('Candarai.ttf', 'CandaraLL', 'italic', 'StandardEncoding');
     doc.addFont('Candarab.ttf', 'CandaraLL', 'bold', 'WinAnsiEncoding');
     //doc.addFont('gothic.ttf', 'LiLing', 'normal', 'Identity-H');
     //doc.setFont('MsGothic');
