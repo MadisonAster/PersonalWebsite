45ec724bca317ffb6237520d275cc54611c9a2db
Madison Aster <info@MadisonAster.com>
2017-06-14 22:32:26 -0700

    cv testing31

--- _js/PDFGeneration.js
+++ _js/PDFGeneration.js
@@ -470,12 +470,20 @@ function CVContactDetails(doc, PositionTitle) {
     //doc.setFont('MsGothic');
     //doc.text(50,50,'Hello World');
     doc.setFont('Candara');
+    
+
+    doc.setDefaultFonts(0, 'Candara');    //English default
+    //doc.setDefaultFonts(1, 'MagicR');    //Korean default
+    doc.setDefaultFonts(3, 'MsGothic');         //Chinese default
+    doc.setDefaultFonts(2, 'MsGothic');        //Japanese default
+    
 
     doc.setFontSize(20);
     doc.setTextColor(255, 0, 0);
+    doc.setCharSpace(0);
     //doc.setFontStyle('bold');
-    doc.text(200, 30, '.thomas mcvay');
-    //doc.drawText(200, 30, ['nanana', {text: '.thomas mcvay', fontSize: 20, TextColor: [255, 0, 0], charSpace: 3, font: 'Candara'},'hahaha']);
+    //doc.text(200, 30, '.thomas mcvay');
+    doc.drawText(200, 30, ['nanana', {text: '.thomas mcvay', fontSize: 20, TextColor: [255, 0, 0], charSpace: 0, font: 'Candara'},'hahaha']);
 
     doc.setFontSize(18);
     doc.setFontStyle('italic');
