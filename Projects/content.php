70159b607ecd3c6481884a22cb945b8f2b06e3e2
Madison Aster <info@MadisonAster.com>
2017-06-25 21:52:38 -0700

    cv testing70

--- Projects/content.php
+++ Projects/content.php
@@ -1,20 +1,18 @@
 <?php
 $content = file("./Projects/content.html");
+foreach ($content as &$value) {
+    if(strpos($value,"{contentArea1}") !== false){
+    }else{
+        echo $value;
+    };
+};
+
 $blacklist = array('.', '..');
 //$dirs = array_filter(glob('./_Assets/Projects/*'), 'is_dir');
 
-echo "<div class='ContentDiv'>\n";
-echo "HelloWorld!!!!!!!!\n";
-echo "</div>\n";
+//echo "<div class='ContentDiv'>\n";
+//echo "HelloWorld!!!!!!!!\n";
+//echo "</div>\n";
 
-/*        
-foreach ($dirs as &$dir){
-    if(!in_array($dir, $blacklist)){
-        echo "<div class='ContentDiv'>\n";
-        echo $dir."HelloWorld2!!\n";
-        //include $dir.'/info.php';
-        echo "</div>\n";
-    };
-};
-*/
+
 ?>
