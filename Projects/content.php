246bf1d6d11c4b85d5939256c6b214a438d4d79a
Madison Aster <info@MadisonAster.com>
2017-06-25 21:36:07 -0700

    cv testing67

--- Projects/content.php
+++ Projects/content.php
@@ -4,9 +4,9 @@ $dirs = array_filter(glob('./_Assets/Projects/*'), 'is_dir');
 
 foreach ($dirs as &$dir){
     if(!in_array($dir, $blacklist)){
-        echo"<div class='ContentDiv'>\n";
-        echo"HelloWorld2!!\n";
-        include $dir.'/info.php';
+        echo "<div class='ContentDiv'>\n";
+        echo $dir."HelloWorld2!!\n";
+        //include $dir.'/info.php';
         echo "</div>\n";
     };
 };
