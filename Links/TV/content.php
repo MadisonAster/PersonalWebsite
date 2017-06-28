484e208e8203351b09c70ff7c3c72a30708b7eca
Madison Aster <info@MadisonAster.com>
2017-06-27 23:35:36 -0700

    cv testing116

--- Links/TV/content.php
+++ Links/TV/content.php
@@ -49,7 +49,7 @@ foreach ($TVdirs as &$dir){
         $infoArray['Description'] = str_replace('\x94', '"', $infoArray['Description']);
         $infoArray['Description'] = str_replace('\x97', "", $infoArray['Description']);
         
-        /*
+        
         echo "<figure id='fig_movInfo_$varName' class='picture-item' style='margin-left:0;' data-groups=\"['none']\" data-added='".$infoArray['Added']."' data-released='".$infoArray['Released']."' data-title='".$infoArray['Title']."' data-rating='".$infoArray['Ratings']['Votes']."' data-imdbscore='".$infoArray['Ratings']['IMDB']."'>\n";
         echo "    <div id='movInfo_$varName' style='width:100%;height:305px;background-color:#111111;padding:1%;display:none;overflow-y:scroll;'>\n";
         
@@ -134,8 +134,6 @@ foreach ($TVdirs as &$dir){
         echo "    </div>\n";
         echo "    </a>\n";
         echo "</figure>\n";
-        */
-        
     };
 };
 
