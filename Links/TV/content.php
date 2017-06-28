d7d9daef09021b6be27d1a5378ee6a76f5f640f3
Madison Aster <info@MadisonAster.com>
2017-06-27 22:22:27 -0700

    cv testing102

--- Links/TV/content.php
+++ Links/TV/content.php
@@ -12,9 +12,12 @@ Sort:
     <option value='imdbscore'>IMDBscore</option>
 </select>
 <div id='TVGrid' class='row-fluid' style='width:100%;margins:0;padding:0;'>
+
 <?php
 $blacklist = array('.', '..');
+$dirs = array_filter(glob('./_Assets/WatchList/TV/*'), 'is_dir');
 ?>
+
 <!-- ?  <php
 $blacklist = array('.', '..');
 //$dirs = array_filter(glob('../../_Assets/WatchList/TV/*'), 'is_dir');
