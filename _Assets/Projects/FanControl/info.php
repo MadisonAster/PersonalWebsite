c050f2b78b06aab049297f1b65f010b94920d388
Madison Aster <info@MadisonAster.com>
2017-06-28 19:40:02 -0700

    cv testing125

--- _Assets/Projects/FanControl/info.php
+++ _Assets/Projects/FanControl/info.php
@@ -1,4 +1,4 @@
 <?php
-echo"<img src='_Assets/Projects/FanControl/thumb.png' style='width:100%;'></img>";
+echo"<img src='".$Projectdir."/thumb.png' style='width:100%;'></img>";
 echo"Fan Control Project Description!";
 ?>
