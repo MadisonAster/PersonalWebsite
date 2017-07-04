71edcd55d1d3c79573483656de911e20addcbea3
Madison Aster <info@MadisonAster.com>
2017-07-03 22:33:08 -0700

    Projects page testing 23

--- _Assets/Projects/360RenderTests/info.php
+++ _Assets/Projects/360RenderTests/info.php
@@ -4,10 +4,10 @@ $ShortDescription = file("./_Assets/Projects/360RenderTests/Description.txt");
 foreach ($ProjectContent as &$ProjectValue) {
     if(strpos($ProjectValue,"{ShortDescription}") !== false){
         foreach ($ShortDescription as &$ShortDescriptionLine) {
-            echo $ShortDescriptionLine;
+            echo $ShortDescriptionLine.'\n';
         };
     }else{
-        echo $ProjectValue;
+        echo $ProjectValue.'\n';
     };
 };
 ?>
