701279f286eb68b5c374d02990b57ca23cd89af0
Madison Aster <info@MadisonAster.com>
2017-09-17 01:08:55 -0700

    background scene replacement testing 08

--- _js/BackgroundScene/files/assets/8044741/1/html-handler.js
+++ _js/BackgroundScene/files/assets/8044741/1/html-handler.js
@@ -1,4 +1,5 @@
 var runOnScroll = function(evt) {     
+    /*
     var cam = pc.app.root.findByName('camera');
     cam.setPosition(cam.getPosition().x, cam.getPosition().y+(evt.wheelDelta/1200), cam.getPosition().z);  
     //console.log(cam.script.dof.far);
@@ -6,8 +7,8 @@ var runOnScroll = function(evt) {
     newfar = Math.max(2, Math.min(newfar, 100));
     cam.script.dof.onAttributeChanged('far', newfar);
     cam.script.dof.far = newfar;
-    
-    //console.log(evt.wheelDelta);
+    */
+    console.log(evt.wheelDelta);
 };
-//window.addEventListener("mousewheel", runOnScroll);
+window.addEventListener("mousewheel", runOnScroll);
 //window.addEventListener("scroll", runOnScroll);
