8992e8244e4544b8174105d8859f42824ad0bdc2
Madison Aster <info@MadisonAster.com>
2017-09-17 01:51:19 -0700

    background scene replacement testing 19

--- _js/BackgroundScene/files/assets/9168228/1/htmlHandler.js
+++ _js/BackgroundScene/files/assets/9168228/1/htmlHandler.js
@@ -21,11 +21,11 @@ var runOnScroll = function(evt) {
     
     var cam = pc.app.root.findByName('camera');
     console.log(cam.getPosition().y);
-    var newy = cam.getPosition().y+(evt.wheelDelta/1200);
+    var newy = cam.getPosition().y+(evt.wheelDelta/1200*2);
     newy = Math.max(-0.75, Math.min(newy, 0.454));
     cam.setPosition(cam.getPosition().x, newy, cam.getPosition().z);  
     //console.log(cam.script.dof.far);
-    var newfar = cam.script.dof.far+evt.wheelDelta/120*2;
+    var newfar = cam.script.dof.far+evt.wheelDelta/120*8;
     newfar = Math.max(1, Math.min(newfar, 100));
     cam.script.dof.onAttributeChanged('far', newfar);
     cam.script.dof.far = newfar;
