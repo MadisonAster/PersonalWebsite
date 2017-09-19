2ab3b3e41ab415ed07b0d7786f79c317cfcd165a
Madison Aster <info@MadisonAster.com>
2017-09-19 01:13:44 -0700

    iframe testing 03

--- _js/iframeFunctions.js
+++ _js/iframeFunctions.js
@@ -1,6 +1,6 @@
 function resizeIframe(obj) {
     if(navigator.userAgent.toLowerCase() .indexOf('firefox') > -1) {
-        height = obj.contentWindow.document.body.scrollHeight+2000
+        height = obj.contentWindow.document.body.offsetHeight+20
         obj.style.height = height+'px';
     } else {
         height = obj.contentWindow.document.body.scrollHeight+20
