965858e50911267949f243f5f0945ed4aee43955
Madison Aster <info@MadisonAster.com>
2017-09-18 19:45:22 -0700

    pdf fix 12

--- _js/PDFGeneration.js
+++ _js/PDFGeneration.js
@@ -69,7 +69,7 @@ function ResumeSetup(){
     doc.setTextColor(89, 92, 98);
     
     doc.setFillColor(255, 0, 0);
-    doc.rect(0, 0, 500, 100, 'F');
+    doc.rect(0, 0, doc.internal.pageSize.width, 25, 'F');
     
     return doc;
 }
