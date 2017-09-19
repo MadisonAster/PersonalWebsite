a2d55e0880b2196ee3da6499daef4a2d95d5c99d
Madison Aster <info@MadisonAster.com>
2017-09-18 22:04:41 -0700

    resize testing 23

--- _js/BackgroundScene/files/assets/9168228/1/htmlHandler.js
+++ _js/BackgroundScene/files/assets/9168228/1/htmlHandler.js
@@ -2,13 +2,14 @@ var HtmlHandler = pc.createScript('htmlHandler');
 
 // initialize code called once per entity
 HtmlHandler.prototype.initialize = function() {
-    var viewportHeight = $(window).height();
+    var viewportHeight = screen.height;
     var HEIGHT_CHANGE_TOLERANCE = 100;
     var bg = $("#application-canvas");
     function resizeBackground() {
         updateSize();
-        if (Math.abs(viewportHeight - $(window).height()) > HEIGHT_CHANGE_TOLERANCE) {
-            viewportHeight = $(window).height();
+        if (viewportHeight != screen.height) {
+        //if (Math.abs(viewportHeight - $(window).height()) > HEIGHT_CHANGE_TOLERANCE) {
+            viewportHeight = screen.height;
             updateSize();
         };
     };
@@ -18,10 +19,10 @@ HtmlHandler.prototype.initialize = function() {
         //alert(screen.height);              ff 678       cr 732
         //alert(document.body.clientHeight); ff 1456 1571 cr 1436
         //alert($(window).outerHeight());    ff 1456 1571 cr 1436
+        //alert($(window).height());         ff 1456 1571 cr 1436
         //alert($(document).height());       ff 6942 7056 cr 6393
-        
-        alert('g '+$(window).height());
-        bg.height(window.outerHeight);
+        alert('h '+screen.height);
+        bg.height(screen.height);
         bg.width(window.width);
         //bg.height(window.outerHeight);
         //bg.height(screen.availHeight);
