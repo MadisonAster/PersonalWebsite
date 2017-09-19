059b33c875b1030aed66f48ada833f60ab700bf6
Madison Aster <info@MadisonAster.com>
2017-09-19 01:22:12 -0700

    iframe testing 06

--- _js/iframeFunctions.js
+++ _js/iframeFunctions.js
@@ -1,6 +1,14 @@
+function getDocHeight(D) {
+    return Math.max(
+        Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+        Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+        Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+    );
+}
+
 function resizeIframe(obj) {
     if(navigator.userAgent.toLowerCase() .indexOf('firefox') > -1) {
-        height = obj.contentWindow.document.body.clientHeight+20;
+        height = getDocHeight(obj.contentWindow.document)+20;
         obj.style.height = height+'px';
     } else {
         height = obj.contentWindow.document.body.scrollHeight+20;
