f95ed6d03ff30dca53d6439bdd2eb95a1405a78e
Madison Aster <info@MadisonAster.com>
2017-09-19 16:40:50 -0700

    projects testing 19

--- _js/slideshow.js
+++ _js/slideshow.js
@@ -1,7 +1,7 @@
-$("#slideshow > div:gt(0)").hide();
+$(".slideshow > div:gt(0)").hide();
 
 setInterval(function() {
-  $('#slideshow > div:first')
+  $('.slideshow > div:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
