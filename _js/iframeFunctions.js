f84d6b06fb177c1d5706d51ba707e8aaf7d31bae
Madison Aster <info@MadisonAster.com>
2017-09-19 01:43:36 -0700

    iframe link testing 04

--- _js/iframeFunctions.js
+++ _js/iframeFunctions.js
@@ -15,3 +15,35 @@ function resizeIframe(obj) {
         obj.style.height = height+'px';
     }
 };
+
+// parent_on_message(e) will handle the reception of postMessages (a.k.a. cross-document messaging or XDM).
+function parent_on_message(e) {
+    // You really should check origin for security reasons
+    // https://developer.mozilla.org/en-US/docs/DOM/window.postMessage#Security_concerns
+    //if (e.origin.search(/^http[s]?:\/\/.*\.localhost/) != -1
+    //    && !($.browser.msie && $.browser.version <= 7)) {
+        var returned_pair = e.data.split('=');
+        if (returned_pair.length != 2){
+            return;
+        }
+        if (returned_pair[0] === 'message-for-parent') {
+            alert(returned_pair[1]);
+            window.history.pushState('obj', 'newtitle', returned_pair[1]);
+        }else{
+            console.log("Parent received invalid message");
+        }
+
+    //}
+}
+
+jQuery(document).ready(function($) {
+    // Setup XDM listener (except for IE < 8)
+    if (!($.browser.msie && $.browser.version <= 7)) {
+        // Connect the parent_on_message(e) handler function to the receive postMessage event
+        if (window.addEventListener){
+            window.addEventListener("message", parent_on_message, false);
+        }else{
+            window.attachEvent("onmessage", parent_on_message);
+        }
+    }
+});
