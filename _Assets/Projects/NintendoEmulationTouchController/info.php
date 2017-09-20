f89c20e4a7d2c9bfea7a28e66bd3ce9071ed3873
Madison Aster <info@MadisonAster.com>
2017-09-19 20:55:08 -0700

    projects testing 37

---
+++ _Assets/Projects/NintendoEmulationTouchController/info.php
@@ -0,0 +1,14 @@
+<?php
+$ProjectContent = file("./_Assets/Projects/FanControl/content.html");
+$ShortDescription = file("./_Assets/Projects/FanControl/Description.txt");
+foreach ($ProjectContent as &$ProjectValue) {
+    if(strpos($ProjectValue,"{ShortDescription}") !== false){
+        foreach ($ShortDescription as &$ShortDescriptionLine) {
+            echo $ShortDescriptionLine;
+            echo '<br/><br/>';
+        };
+    }else{
+        echo $ProjectValue;
+    };
+};
+?>
