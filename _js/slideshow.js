f4552f76e917a7727125a3e6cfe6ebd7349abe74
Madison Aster <info@MadisonAster.com>
2017-09-19 20:06:09 -0700

    projects testing 36

--- _js/slideshow.js
+++ _js/slideshow.js
@@ -1,22 +1,10 @@
 $(".slideshow > div:gt(0)").hide();
 
 setInterval(function() {
-    /*
-    $('.slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-    */
-    //$('.slideshow > div:first').each(function(){
-    //    $(this).appendTo($(this).parent());
-    //});
     var adiv = $('.slideshow > div:first');
     adiv.fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
     .appendTo($(this).parent());
-    //.appendTo('#slideshow');
 }, 3000);
