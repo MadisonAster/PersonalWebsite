a427a13725b11d6e45ec38b13c9bd29ecd6b6dd1
Madison Aster <info@MadisonAster.com>
2017-09-23 20:05:54 -0700

    slideshow testing 48

--- _js/BackgroundScene/files/assets/9168228/1/htmlHandler.js
+++ _js/BackgroundScene/files/assets/9168228/1/htmlHandler.js
@@ -35,7 +35,6 @@ HtmlHandler.prototype.initialize = function() {
     };
     updateSize();
     $(window).resize(resizeBackground);
-    animationLoop();
 };
 
 // update code called every frame
@@ -50,34 +49,6 @@ HtmlHandler.prototype.update = function(dt) {
     cam.script.dof.onAttributeChanged('far', farval); //why?
     cam.script.dof.far = farval;
 };
-
-var runOnScroll = function(evt) {     
-    /*
-    var cam = pc.app.root.findByName('camera');
-    var ypos = Math.min(getYPosition(), 1540);
-    
-    var camypos = ypos/-1400+0.454;
-    cam.setPosition(cam.getPosition().x, camypos, cam.getPosition().z); 
-    
-    var farval = 2*(Math.min(ypos, 385))/-3.85+200+1;
-    cam.script.dof.onAttributeChanged('far', farval); //why?
-    cam.script.dof.far = farval;
-    */
-};
-function animationLoop(){
-    requestAnimFrame(animationLoop);
-    runOnScroll();
-};
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame || 
-    window.webkitRequestAnimationFrame || 
-    window.mozRequestAnimationFrame || 
-    window.oRequestAnimationFrame ||  
-    window.msRequestAnimationFrame || 
-    function(callback){
-		window.setTimeout(callback, 1000.0/window.frameRate);
-    };
-})();
 
 function getYPosition(){
 	return (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
