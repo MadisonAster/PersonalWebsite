4ed632300b09d47555fc3d776fde29b0c18297e7
Madison Aster <info@MadisonAster.com>
2017-09-24 23:00:19 -0700

    hiding projects behavior2

--- Projects/content.php
+++ Projects/content.php
@@ -18,7 +18,7 @@ foreach($Projectdirs as &$Projectdir){
         $dirArray = explode("/", $Projectdir);
         $folderName = current(array_slice($dirArray, -1));
         $ProjectTitle = substr($folderName, 3);
-        if(substr($folderName, 0, 1) !== '_')){
+        if(substr($folderName, 0, 1) !== '_'){
             echo "<div class='ContentDiv'>\n";
             echo "<h2 style='text-align:left;'>".$ProjectTitle."</h2>\n";
             include $Projectdir."/info.php";
