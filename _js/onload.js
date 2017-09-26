cf5c26fffcdfa9a698e498d4686004a76ae0985f
Madison Aster <info@MadisonAster.com>
2017-09-26 00:20:26 -0700

    scroll disable testing 5

--- _js/onload.js
+++ _js/onload.js
@@ -1,39 +1,7 @@
-function preventDefault(e) {
-  e = e || window.event;
-  if (e.preventDefault)
-      e.preventDefault();
-  e.returnValue = false;  
-}
-
-function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-    }
-}
-function disableScroll() {
-  if (window.addEventListener) // older FF
-      window.addEventListener('DOMMouseScroll', preventDefault, false);
-  window.onwheel = preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-  window.ontouchmove  = preventDefault; // mobile
-  document.onkeydown  = preventDefaultForScrollKeys;
-}
-
-function enableScroll() {
-    if (window.removeEventListener)
-        window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.onmousewheel = document.onmousewheel = null; 
-    window.onwheel = null; 
-    window.ontouchmove = null;  
-    document.onkeydown = null;  
-}
-
 window.onload = function(){
     //backgroundOnload();
     //sorttable.init();
     window.scrollTo(0,1);
-    //$('body').addClass('stop-scrolling');
     menuArray = document.getElementById('Menu').childNodes;
     for(var i=0;i<menuArray.length;i++){
         if(menuArray[i].id != null){
