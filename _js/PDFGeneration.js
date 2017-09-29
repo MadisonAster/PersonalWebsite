0359c078d589f3dd31b10575f35cd7613c7e2591
Madison Aster <info@MadisonAster.com>
2017-09-28 23:47:25 -0700

    cvgen testing 47

--- _js/PDFGeneration.js
+++ _js/PDFGeneration.js
@@ -514,12 +514,15 @@ function CVContactDetails(doc, PositionTitle) {
     doc.drawText(201, 120, '.'+GetPhone());
     doc.drawText(201, 140, GetEmail());
     doc.drawText(201, 160, 'www.thomas-mcvay.info');
-    imgData1 = getBase64FromImageUrl('./_Assets/CVThumb.jpg');
-    //imgData1 = getBase64FromImageUrl('./_Assets/Projects/008Unreal 360 Render Tests/thumb.jpg');
-    doc.addImage(imgData1, 'JPEG', 1, 78, 178, 100);
+    
 }
 function CVPage(doc) {
     //console.log(Projects);
+    imgData1 = getBase64FromImageUrl("./_Assets/CVThumb.jpg");
+    //imgData1 = getBase64FromImageUrl('./_Assets/Projects/008Unreal 360 Render Tests/thumb.jpg');
+    doc.addImage(imgData1, 'JPEG', 1, 78, 178, 100);
+    
+    
     var ImageYPosition = 220;
     var TextYPosition = 200;
 
