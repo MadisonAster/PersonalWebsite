c87cdfb7591d38a34c9f2ab411469c617e4c1835
Madison Aster <info@MadisonAster.com>
2017-10-02 00:08:28 -0700

    reload testing 14

--- _js/iframeFunctions.js
+++ _js/iframeFunctions.js
@@ -8,8 +8,8 @@ function getDocHeight(D) {
 
 function resizeIframe(obj) {
     if(navigator.userAgent.toLowerCase() .indexOf('firefox') > -1) {
-        height = getDocHeight(obj.contentWindow.document)+2000;
-        obj.style.height = height+'px';
+        //height = getDocHeight(obj.contentWindow.document)+2000;
+        //obj.style.height = height+'px';
     } else {
         height = obj.contentWindow.document.body.scrollHeight+20;
         obj.style.height = height+'px';
