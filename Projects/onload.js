60d8d97a23a895b2069bb56429996da16fab248b
Madison Aster <info@MadisonAster.com>
2017-10-02 17:41:53 -0700

    pagination link removal 06

---
+++ Projects/onload.js
@@ -0,0 +1,8 @@
+function Projects_onload(){
+    U360PageLinks = $('#U360Pages').children();
+    console.log(U360PageLinks);
+    for(var i=0;i<U360PageLinks.length;i++){
+        console.log(U360PageLinks[i]);
+        $(U360PageLinks[i]).attr('onclicked', 'loadframe('+(i+1)+');');
+    };
+};
