429ee5587c8061627fd76678589aad0e7af6eb07
Madison Aster <info@MadisonAster.com>
2017-10-02 21:59:22 -0700

    Dialog Styling 23

--- _js/PDFGeneration.js
+++ _js/PDFGeneration.js
@@ -26,9 +26,9 @@ function CVDialog() {
     $('#DialogFocuser').css('display', 'block');
     $('#CVDialog').css('width', '50%');
     $('#CVDialog').css('height', '25%');
-    $('#CVDialog').css('min-height', '400px');
+    $('#CVDialog').css('min-height', '8cm');
     $('#CVDialog').css('max-height', '500px');
-    $('#CVDialog').css('min-width', '500px');
+    $('#CVDialog').css('min-width', '16cm');
     $('#CVDialog').css('max-width', '600px')
 }
 function CloseResumeDialog() {
