69bf0bfe2eb0efde36d5bbe9e70f5762112a1db9
Madison Aster <info@MadisonAster.com>
2017-10-17 15:52:50 -0700

    testing backend 02

--- DemoReel/VSAudio.js
+++ DemoReel/VSAudio.js
@@ -1,10 +1,10 @@
 aSources = [
-    './_Assets/VSAudio/tiger.mp3',
-    './_Assets/VSAudio/girl.mp3',
-    './_Assets/VSAudio/vader.mp3',
+'./_Assets/VSAudio/girl.mp3',
+ './_Assets/VSAudio/tiger.mp3',
+ './_Assets/VSAudio/vader.mp3'
 ];
 aSourceDurations = {
-    './_Assets/VSAudio/girl.mp3':184710,
-    './_Assets/VSAudio/tiger.mp3':144090,
-    './_Assets/VSAudio/vader.mp3':255290,
+'./_Assets/VSAudio/girl.mp3': 184711.837,
+ './_Assets/VSAudio/tiger.mp3': 144091.429,
+ './_Assets/VSAudio/vader.mp3': 255294.694
 };
--- DemoReel/VSVideo.js
+++ DemoReel/VSVideo.js
@@ -1,21 +1,12 @@
 vSources = [
-    './_Assets/VSVideo/tiger.mp4',
-    './_Assets/VSVideo/girl.mp4',
-    './_Assets/VSVideo/vader.mp4',
-    './_Assets/VSVideo/tiger.mp4',
-    './_Assets/VSVideo/girl.mp4',
-    './_Assets/VSVideo/vader.mp4',
-    './_Assets/VSVideo/tiger.mp4',
-    './_Assets/VSVideo/girl.mp4',
-    './_Assets/VSVideo/vader.mp4',
-    './_Assets/VSVideo/tiger.mp4',
-    './_Assets/VSVideo/girl.mp4',
+'./_Assets/VSVideo/SacramentMountains.mp4',
+ './_Assets/VSVideo/girl.mp4',
+ './_Assets/VSVideo/tiger.mp4',
+ './_Assets/VSVideo/vader.mp4'
 ];
 vSourceDurations = {
-    './_Assets/VSVideo/danceforme.mp4': 115620,
-    './_Assets/VSVideo/girl.mp4':20080,
-    './_Assets/VSVideo/koch.mp4':149620,
-    './_Assets/VSVideo/olympia.mp4':98400,
-    './_Assets/VSVideo/tiger.mp4':8040,
-    './_Assets/VSVideo/vader.mp4':20000,
+'./_Assets/VSVideo/SacramentMountains.mp4': 2791.992,
+ './_Assets/VSVideo/girl.mp4': 20080.0,
+ './_Assets/VSVideo/tiger.mp4': 8039.999999999999,
+ './_Assets/VSVideo/vader.mp4': 20000.0
 };
