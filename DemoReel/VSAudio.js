a856025219f47e6e4696bb0e97b3ccbfc2887f8a
Madison Aster <info@MadisonAster.com>
2017-10-17 15:00:29 -0700

    testing backend 01

---
+++ DemoReel/VSAudio.js
@@ -0,0 +1,10 @@
+aSources = [
+    './_Assets/VSAudio/tiger.mp3',
+    './_Assets/VSAudio/girl.mp3',
+    './_Assets/VSAudio/vader.mp3',
+];
+aSourceDurations = {
+    './_Assets/VSAudio/girl.mp3':184710,
+    './_Assets/VSAudio/tiger.mp3':144090,
+    './_Assets/VSAudio/vader.mp3':255290,
+};
---
+++ DemoReel/VSVideo.js
@@ -0,0 +1,21 @@
+vSources = [
+    './_Assets/VSVideo/tiger.mp4',
+    './_Assets/VSVideo/girl.mp4',
+    './_Assets/VSVideo/vader.mp4',
+    './_Assets/VSVideo/tiger.mp4',
+    './_Assets/VSVideo/girl.mp4',
+    './_Assets/VSVideo/vader.mp4',
+    './_Assets/VSVideo/tiger.mp4',
+    './_Assets/VSVideo/girl.mp4',
+    './_Assets/VSVideo/vader.mp4',
+    './_Assets/VSVideo/tiger.mp4',
+    './_Assets/VSVideo/girl.mp4',
+];
+vSourceDurations = {
+    './_Assets/VSVideo/danceforme.mp4': 115620,
+    './_Assets/VSVideo/girl.mp4':20080,
+    './_Assets/VSVideo/koch.mp4':149620,
+    './_Assets/VSVideo/olympia.mp4':98400,
+    './_Assets/VSVideo/tiger.mp4':8040,
+    './_Assets/VSVideo/vader.mp4':20000,
+};
