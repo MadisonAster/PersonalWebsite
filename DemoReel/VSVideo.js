69f37ffccd72de446dc5efd72e0d3972997cf7be
Madison Aster <info@MadisonAster.com>
2017-10-17 20:08:20 -0700

    finished mabeline clips

--- DemoReel/VSVideo.js
+++ DemoReel/VSVideo.js
@@ -4,6 +4,9 @@ vSources = [
  './_Assets/VSVideo/MSMoveJump.mp4',
  './_Assets/VSVideo/Mabeline01.mp4',
  './_Assets/VSVideo/Mabeline02.mp4',
+ './_Assets/VSVideo/Mabeline03.mp4',
+ './_Assets/VSVideo/Mabeline04.mp4',
+ './_Assets/VSVideo/Mabeline05.mp4',
  './_Assets/VSVideo/SacramentMountains.mp4',
  './_Assets/VSVideo/SapphireEye.mp4'
 ];
@@ -13,6 +16,9 @@ vSourceDurations = {
  './_Assets/VSVideo/MSMoveJump.mp4': 666.9920000000001,
  './_Assets/VSVideo/Mabeline01.mp4': 4166.991999999999,
  './_Assets/VSVideo/Mabeline02.mp4': 6541.991999999999,
+ './_Assets/VSVideo/Mabeline03.mp4': 4166.991999999999,
+ './_Assets/VSVideo/Mabeline04.mp4': 4166.991999999999,
+ './_Assets/VSVideo/Mabeline05.mp4': 6416.991999999999,
  './_Assets/VSVideo/SacramentMountains.mp4': 2791.992,
  './_Assets/VSVideo/SapphireEye.mp4': 708.9839999999999
 };
