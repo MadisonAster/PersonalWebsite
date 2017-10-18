4d8d37a9ce3685cef3e88610f7b42264334fbe81
Madison Aster <info@MadisonAster.com>
2017-10-17 18:27:17 -0700

    clip ingest

--- DemoReel/VSVideo.js
+++ DemoReel/VSVideo.js
@@ -1,12 +1,18 @@
 vSources = [
-'./_Assets/VSVideo/SacramentMountains.mp4',
- './_Assets/VSVideo/girl.mp4',
- './_Assets/VSVideo/tiger.mp4',
- './_Assets/VSVideo/vader.mp4'
+'./_Assets/VSVideo/FollowCar.mp4',
+ './_Assets/VSVideo/GorillaGlue.mp4',
+ './_Assets/VSVideo/MSMoveJump.mp4',
+ './_Assets/VSVideo/Mabeline01.mp4',
+ './_Assets/VSVideo/Mabeline02.mp4',
+ './_Assets/VSVideo/SacramentMountains.mp4',
+ './_Assets/VSVideo/SapphireEye.mp4'
 ];
 vSourceDurations = {
-'./_Assets/VSVideo/SacramentMountains.mp4': 2791.992,
- './_Assets/VSVideo/girl.mp4': 20080.0,
- './_Assets/VSVideo/tiger.mp4': 8039.999999999999,
- './_Assets/VSVideo/vader.mp4': 20000.0
+'./_Assets/VSVideo/FollowCar.mp4': 6375.0,
+ './_Assets/VSVideo/GorillaGlue.mp4': 1958.9840000000002,
+ './_Assets/VSVideo/MSMoveJump.mp4': 666.9920000000001,
+ './_Assets/VSVideo/Mabeline01.mp4': 4166.991999999999,
+ './_Assets/VSVideo/Mabeline02.mp4': 6541.991999999999,
+ './_Assets/VSVideo/SacramentMountains.mp4': 2791.992,
+ './_Assets/VSVideo/SapphireEye.mp4': 708.9839999999999
 };
