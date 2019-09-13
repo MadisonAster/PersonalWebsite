61808213d7c1537137630c242791465e348d9266
Madison Aster <info@MadisonAster.com>
2019-09-13 11:04:49 -0700

    starting MazeRunnerBG module, lots to do

---
+++ _js/MazeRunnerBG.js
@@ -0,0 +1,89 @@
+var heightRatio = getDocHeightBG()/1080;
+var frameRate = 30;
+var imageCache = new Array; // stores all of the frames for quick access
+var frameLoadingOrder = new Array;
+var currentFrame = false;
+window.onresize = function(event) {
+    console.log('onresize');
+};
+
+function zFill(number, length){
+	var str = '' + number; 
+	while(str.length < length){
+		str = '0' + str;
+	};
+	return str;
+};
+
+function getDocWidth(){
+    return document.documentElement.clientWidth;
+};
+function getDocHeightBG(){
+    return document.documentElement.clientHeight;
+};
+function getYPosition(){
+	return (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
+};
+function videoSupportCheck(){
+   canPlay = false;
+   v = document.createElement('video');
+   if(v.canPlayType && v.canPlayType('video/mp4').replace(/no/, '')) {
+       canPlay = true;
+   };
+   return canPlay;
+};
+
+function zoomCrop(targetElement){
+    var image_width = 1920.0;
+	var image_height = 1080.0;
+	var height_ratio = image_height / getDocHeightBG();
+	var width_ratio = image_width / getDocWidth();
+	
+	if (height_ratio < width_ratio){
+        targetElement.style['width'] = 'auto';
+        targetElement.style['height'] = '100%';
+        
+        computedRatio = getDocHeightBG()/image_height;
+        computedWidth = image_width*computedRatio;
+        
+        difference = computedWidth-getDocWidth();
+        offset = (difference/2)*-1;
+        targetElement.style['left'] = offset+'px';
+        targetElement.style['top'] = 0;
+	}else{
+        targetElement.style['height'] = 'auto';
+		targetElement.style['width'] = '100%';
+        
+		computedRatio = getDocWidth()/image_width;
+        computedHeight = image_height*computedRatio;
+        
+		difference = computedHeight-getDocHeightBG();
+        offset = (difference/2)*-1;
+        targetElement.style['top'] = offset+'px';
+        targetElement.style['left'] = 0;
+	};
+};
+
+function animationLoop(){
+    requestAnimFrame(animationLoop);
+    //targetFrame = parseInt(getYPosition()/window.heightRatio+window.startFrame);
+    //if(targetFrame > window.endFrame){
+    //    targetFrame = window.endFrame;
+    //};
+    //findFrame(targetFrame);
+};
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame || 
+    window.mozRequestAnimationFrame || 
+    window.oRequestAnimationFrame ||  
+    window.msRequestAnimationFrame || 
+    function(callback){
+		window.setTimeout(callback, 1000.0/window.frameRate);
+    };
+})();
+
+function backgroundOnload(){
+    animationLoop();
+};
