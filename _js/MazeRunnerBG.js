b0cd901a36155a096c508d24fdac575543894d69
Madison Aster <info@MadisonAster.com>
2020-05-16 20:37:38 -0700

    adding demo code

--- _js/MazeRunnerBG.js
+++ _js/MazeRunnerBG.js
@@ -91,7 +91,7 @@ class MazeRunner {
         console.log('MazeRunner.init');
         this.renderer = new THREE.WebGLRenderer();
         this.controls = ScrollControls();
-        this.Maze = GenerateMaze();
+        this.Maze = this.GenerateMaze();
     };
     GenerateMaze(){
         //this.MazeGeo = somefunction;
@@ -113,12 +113,46 @@ class MazeRunner {
         
     };
     update(){
-        console.log('updoot!');
+        //console.log('updoot!');
     };
 };
 
 function backgroundOnload(){
     console.log('MazeRunnerBG.backgroundOnload');
-    window.MyMaze = new MazeRunner();
-    animationLoop();
+    //window.MyMaze = new MazeRunner();
+    //animationLoop();
+    Init();
 };
+
+function Init(){
+    window.DemoMaze = new Windows95Maze(12,12, window.innerWidth, window.innerHeight);
+    window.addEventListener('resize', ResizeHandling);
+    window.addEventListener('keydown', KeyHandling);
+    document.body.appendChild(window.DemoMaze.MazeRenderer.domElement);
+}
+
+function ResizeHandling(){
+    window.DemoMaze.MazeRenderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function KeyHandling(event){
+        switch(event.keyCode)
+        {
+            case 87: //w
+            case 38: //Up
+                window.DemoMaze.Go('f');
+                break;
+            case 65: //a
+            case 37: //Left
+                window.DemoMaze.Turn('l');
+                break;
+            case 83: //s
+            case 40: //Down
+                window.DemoMaze.Go('b');
+                break;
+            case 68: //d
+            case 39: //Right
+                window.DemoMaze.Turn('r');
+                break;
+        }
+}
