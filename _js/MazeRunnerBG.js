af3abde414a83aeff1a4f90c95bde9574da9d272
Madison Aster <info@MadisonAster.com>
2020-05-18 20:07:49 -0700

    phong 28

--- _js/MazeRunnerBG.js
+++ _js/MazeRunnerBG.js
@@ -63,7 +63,7 @@ function MazeInit(){
                                         WallsShininess=2,
                                         
                                         LightColor=0xFFFFFF,
-                                        LightIntensity=1,
+                                        LightIntensity=10,
                                         LightDecay=2.0,
                                         LightDistance=960,
                                         
