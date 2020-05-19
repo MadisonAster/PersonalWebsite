f1ec573bfb2358229042ea1941f370a61792569a
Madison Aster <info@MadisonAster.com>
2020-05-18 20:11:56 -0700

    glow 2

--- _js/MazeRunnerBG.js
+++ _js/MazeRunnerBG.js
@@ -58,7 +58,7 @@ function MazeInit(){
                                         
                                         WallsMaterialType='Phong',
                                         WallsEmissiveColor=0x00FFFF,
-                                        WallsEmissiveIntensity=10,
+                                        WallsEmissiveIntensity=100,
                                         WallsSpecular=0x999999,
                                         WallsShininess=2,
                                         
