4242b2f27abacef6602e12b4bda90caff95d39a9
Madison Aster <info@MadisonAster.com>
2020-05-20 00:31:17 -0700

    left wall 10

--- _js/MazeRunnerBG.js
+++ _js/MazeRunnerBG.js
@@ -119,7 +119,7 @@ function ScrollHandling(event){
         window.DemoMaze.MazeCamera.position.y = y;
         if (y == window.DemoMaze.MazeHeight/2) {
             if (!window.DemoMaze.MazeAutopilot){
-                setTimeout(function(){window.DemoMaze.MazeAutopilot = true;}, 200);
+                setTimeout(function(){window.DemoMaze.MazeAutopilot = true;}, 300);
             };
         } else {
             window.DemoMaze.MazeAutopilot = false;
