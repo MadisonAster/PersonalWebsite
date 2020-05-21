df0a65675ecb7077184b0ace8680ce18e3b570c0
Madison Aster <info@MadisonAster.com>
2020-05-21 14:12:29 -0700

    fixing div names

--- Favorites/Games/content.php
+++ Favorites/Games/content.php
@@ -1,3 +1,3 @@
-<div id='BookmarksDiv' style='padding:0;margin:0;height:1000px;display:none;text-align:center;'>
+<div id='GamesDiv' style='padding:0;margin:0;height:1000px;display:none;text-align:center;'>
 Live access to my favorite games, coming soon!
 </div>
