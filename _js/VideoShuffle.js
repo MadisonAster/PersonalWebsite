33fb3f95fde845b5d7ac7ce2a63f8b4d18af2ce6
Madison Aster <info@MadisonAster.com>
2020-05-20 20:49:13 -0700

    resize 4

--- _js/VideoShuffle.js
+++ _js/VideoShuffle.js
@@ -613,7 +613,7 @@ function ShufflePlayer(vSources, aSources, vSourceDurations, aSourceDurations, a
         }
     };
     this.transition = this.transitions[this.activeTransition];
-    this.resize = this.debounce(function () { //Cludging this till I have time to turn all this into a class later. Why did I do this this way?
+    this.resize = function () { //Cludging this till I have time to turn all this into a class later. Why did I do this this way?
         //Takes:
         //Performs: resizes canvas based on the wrapper width
         //Returns:
@@ -634,7 +634,7 @@ function ShufflePlayer(vSources, aSources, vSourceDurations, aSourceDurations, a
         
         this.canvas.width = width;
         this.canvas.height = height;
-    });
+    };
     this.resize();
     this.loadVideos();
     this.loadAudios();
