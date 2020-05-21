0805f29c4bb2468a2726cd57e77d7e7b4c89fb91
Madison Aster <info@MadisonAster.com>
2020-05-20 21:49:47 -0700

    scrolling 5

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@ foreach ($dirs as &$dir){
     if(!in_array($dir, $blacklist)){
         if(substr($dir, 0, 1) !== '_'){
             echo "    <li id='MenuItem_$dir' class='MenuItem'>";
-            echo "    <a href='#' onclick=\"History.pushState(null, '$dir - Madison Aster', '$dir');return false;\">\n";
+            echo "    <a href='#' onclick=\"History.pushState(null, '$dir - Madison Aster', '$dir');window.scrollTo(0,0);return false;\">\n";
             echo "    $dir\n";
             echo "    </a>\n";
             echo "    </li>\n";
