f1c2a64a2bfc5508649f36d2a3efbf175e1e8483
Madison Aster <info@MadisonAster.com>
2020-05-21 22:28:16 -0700

    snapshots 3

--- _py/takeSnapshots.py
+++ _py/takeSnapshots.py
@@ -9,14 +9,81 @@ def main():
     
     htmlDir = os.path.dirname(os.path.abspath(__file__)).rsplit('_py',1)[0]
     
-    html = getAndSanitizeHTML('http://www.imdb.com/name/nm4807696')    
-    downloadFiles_SaveHTML(html, htmlDir+'IMDB/snapshot')
+    html = GetIMDB('http://www.imdb.com/name/nm4807696', htmlDir+'IMDB/snapshot')    
+    #downloadFiles_SaveHTML(html, htmlDir+'IMDB/snapshot')
     
-    html = getAndSanitizeHTML('http://github.com/MadisonAster')
-    expression = re.compile('<img.*?src=[\'"].*?9937336.*?[\'"]')
-    html = linkreplace(expression, '9937336.jpg', html)
-    downloadFiles_SaveHTML(html, htmlDir+'GitHub/snapshot')
+    #html = getAndSanitizeHTML('http://github.com/MadisonAster')
+    #expression = re.compile('<img.*?src=[\'"].*?9937336.*?[\'"]')
+    #html = linkreplace(expression, '9937336.jpg', html)
+    #downloadFiles_SaveHTML(html, htmlDir+'GitHub/snapshot')
     
+
+def GetIMDB(url, SnapshotFolder):
+    #Takes: url as valid public url
+    #Performs: downloads html response from server
+    #Returns: html as str
+    
+    #########################Load HTML############################
+    domain = url.strip('http://')
+    domain = domain.strip('https://')
+    domain = 'https://'+domain.split('/',1)[0]+'/'
+    
+    response = urllib2.urlopen(url)
+    html = response.read()
+
+    expression = re.compile('<script.*?/script>', flags=re.DOTALL)
+    html = re.sub(expression, '', html)
+
+    expression = re.compile('<iframe.*?/iframe>', flags=re.DOTALL)
+    html = re.sub(expression, '', html)
+    ###############################################################
+    
+    
+    ###################Sanitize HTML text##########################
+    html = html.replace('href="/', 'href="'+domain)
+    html = html.replace("href='/", "href='"+domain)
+    html = html.replace("<a", "<a target='_blank'")
+    
+    html = html.replace(" - as Thomas McVay", "")
+    if '<div id="details-akas"' in html:
+        html2 = html.split('<div id="details-akas"',1)
+        html = html2[0]+html2[1].split('</div>',1)[-1]
+    html = html.replace("Thomas McVay", "")
+    
+    if '<nav' in html:
+        html2 = html.split('<nav',1)
+        html = html2[0]+html2[1].split('</nav>',1)[-1]
+    
+    expression = re.compile('action=[\'"]/.*?[\'"]')
+    html = re.sub(expression, 'action="'+url+'"', html)
+    ###############################################################
+    
+    
+    #########################Save Images###########################
+    for filename in os.listdir(SnapshotFolder):
+        os.remove(SnapshotFolder+'/'+filename)
+    images = html.split('<img')[1:]
+    for i, a in enumerate(images):
+        imgURL = a.split('src="',1)[1].split('"',1)[0]
+        imgExt = imgURL.rsplit('.',1)[-1]
+        if imgURL != '' and imgExt != '': #Do better filename validity check here
+            imgName = 'image_'+str(i).zfill(3)+'.'+imgExt
+            imgFile = urllib2.urlopen(imgURL)
+            fileObject = open(SnapshotFolder+'/'+imgName, 'wb')
+            fileObject.write(imgFile.read())
+            fileObject.close()
+            html = html.replace(imgURL, imgName)
+    ###############################################################
+    
+    
+    #####################create index.html#########################
+    outputpath = SnapshotFolder+'/index.html'
+    file = open(outputpath, 'w')
+    file.write(html)
+    file.close()
+    ###############################################################
+    
+
 def downloadFiles_SaveHTML(html, folder):
     #Takes: html as html string, folder as path to snapshot folder
     #Performs: downloads image files to folder if they don't exist, saves index.html to folder, deletes any files not referenced in html
@@ -51,7 +118,6 @@ def downloadFiles_SaveHTML(html, folder):
     for ExistingImage in ExistingImages:
         if ExistingImage not in imgNames and ExistingImage != '9937336.jpg':
             os.remove(folder+'/'+ExistingImage)
-
 def getAndSanitizeHTML(url):
     #Takes: url as valid public url
     #Performs: downloads html response from server
