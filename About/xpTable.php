62fb9a9c4a2e33288b725605a5a9c231ac492198
Madison Aster <info@MadisonAster.com>
2020-05-25 12:50:33 -0700

    formatting 3

--- About/xpTable.php
+++ About/xpTable.php
@@ -1,21 +1,21 @@
-<table id='ExperienceTable' class='sortable' align='left' style='position:inline;max-width:450px;border:solid 1px white;margin-right:40px;'>
-<tbody>
-<tr style='height:40px;'>
-<th style='width:200px;'>Name</th>
-<th style='width:125px;'>First Use</th>
-<th style='width:125px;'>Skill Level</th>
-</tr>
+        <table id='ExperienceTable' class='sortable' align='left' style='position:inline;max-width:450px;border:solid 1px white;margin-right:40px;'>
+        <tbody>
+        <tr style='height:40px;'>
+        <th style='width:200px;'>Name</th>
+        <th style='width:125px;'>First Use</th>
+        <th style='width:125px;'>Skill Level</th>
+        </tr>
 <?php
 $table = file("./About/xpTable.csv");
 foreach ($table as &$line){
-    echo "<tr>";
+    echo "        <tr>";
     $items = explode(",", $line);
     foreach ($items as $item){
-        echo "<td>";
+        echo "            <td>";
         echo $item;
-        echo "</td>";
+        echo "            </td>";
     };
-    echo "</tr>";
+    echo "        </tr>";
 };
 ?>
 </tbody>
