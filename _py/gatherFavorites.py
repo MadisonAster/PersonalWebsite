b127110e98a3e5f38623f76b683f53057eee93fa
Madison Aster <info@MadisonAster.com>
2020-05-24 18:20:02 -0700

    rawg url

--- _py/gatherFavorites.py
+++ _py/gatherFavorites.py
@@ -17,7 +17,7 @@ def main():
     GetIMDB('https://www.imdb.com/list/ls098333584/?sort=list_order,asc&st_dt=&mode=detail&page=1', MoviesDir)
     #GetIMDB('https://www.imdb.com/list/ls098367816/?sort=list_order,asc&st_dt=&mode=detail&page=1', TVDir)
     #GetGoodReads('https://www.goodreads.com/review/list/50378844-lolmuly?page=1&shelf=read', BooksDir)
-    #GetRawg('', GamesDir)
+    #GetRawg('https://rawg.io/@maddieawesome/games', GamesDir)
     #GetFirefox('', BookmarksDir)
     
 def GetIMDB(url, OutputDir):
