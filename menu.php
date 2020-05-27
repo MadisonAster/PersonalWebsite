13b74f75cc09e00ce4ac5b5b1a585c76d765888c
Madison Aster <info@MadisonAster.com>
2020-05-26 19:09:43 -0700

    menu order 2

--- menu.php
+++ menu.php
@@ -1,19 +1,23 @@
 <ul id="Menu">
 <?php
-$blacklist = array('.', '..', 'stats', 'test', 'testing');
-$dirs = array_filter(glob('*'), 'is_dir');
 $preferredlist = array('About', 'Projects', 'DemoReel', 'Favorites', 'GitHub', 'CodeWars', 'IMDB');
-
-foreach ($preferredlist as &$dir){
+$dirs = array_filter(glob('*'), 'is_dir');
+$blacklist = array('.', '..', 'stats', 'test', 'testing', '.git');
+foreach ($dirs as &$dir){
     if(!in_array($dir, $blacklist)){
         if(substr($dir, 0, 1) !== '_'){
-            echo "    <li id='MenuItem_$dir' class='MenuItem'>";
-            echo "    <a href='#' onclick=\"History.pushState(null, '$dir - Madison Aster', '$dir');window.scrollTo(0,1);return false;\">\n";
-            echo "    $dir\n";
-            echo "    </a>\n";
-            echo "    </li>\n";
+            if(!in_array($dir, $preferredlist)){
+                array_push($preferredlist, $dir);
+            };
         };
     };
+};
+foreach ($preferredlist as &$dir){
+    echo "    <li id='MenuItem_$dir' class='MenuItem'>";
+    echo "    <a href='#' onclick=\"History.pushState(null, '$dir - Madison Aster', '$dir');window.scrollTo(0,1);return false;\">\n";
+    echo "    $dir\n";
+    echo "    </a>\n";
+    echo "    </li>\n";
 };
 ?>
 </ul>
