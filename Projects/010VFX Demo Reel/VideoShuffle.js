1fb64bbcf07bfd5caed39857c38c24e5c94dce01
Madison Aster <info@MadisonAster.com>
2020-05-28 19:19:20 -0700

    demo reel move 32

--- Projects/010VFX Demo Reel/VideoShuffle.js
+++ Projects/010VFX Demo Reel/VideoShuffle.js
@@ -439,9 +439,11 @@ function ShufflePlayer(vSources, aSources, vSourceDurations, aSourceDurations, a
             
             button.style.backgroundSize = 'cover';
             button.style.backgroundRepeat = 'no-repeat';
-            button.style.backgroundImage = 'url('+this.videoSources[i].replace('.mp4', '.jpg')+')';
+            button.style.backgroundImage = 'url('+this.videoSources[i].replace('.mp4', '.jpg').replace('./Projects/', './_testing/Projects/')+')';
             console.log(this.videoSources[i].replace('.mp4', '.jpg'));
             console.log(button);
+            console.log(button.style);
+            console.log(button.style.backgroundImage);
             button.addEventListener('click', this.switchVideo.bind(null, this, i), false);
             this.thumbs.appendChild(button);
             
