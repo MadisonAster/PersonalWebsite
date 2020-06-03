f95bb520e656c9dd1b50f8c3fd12e0bd67b59a0a
Madison Aster <info@MadisonAster.com>
2020-06-03 04:47:28 -0700

    added new thumbnails

---
+++ Favorites/TV/snapshot/Frontline/info.php
@@ -0,0 +1,19 @@
+<?php
+$infoArray = array('Actors' => array('Will Lyman&tv'),
+ 'Added' => '2013-02-23',
+ 'Countries' => array('USA'),
+ 'Description' => '\nFrontline (US): Since its inception, Frontline has never shied away from tough, controversial issues or complex stories. Frontline remains committed to providing a primetime venue for engaging documentaries that fully explore and illuminate the critical issues of our times. ',
+ 'Director' => array(),
+ 'Genres' => array('Documentary&tv', 'History&tv'),
+ 'IMDB' => 'http://www.imdb.com/title/tt0133302',
+ 'PrimeWire' => 'http://www.primewire.ag/tv-139442-Frontline-US',
+ 'Ratings' => array('IMDB' => '8.6/10',
+             'Metascore' => 'N/A',
+             'PrimeWire' => '3.41/5',
+             'RT' => 'N/A',
+             'Votes' => '58'),
+ 'Released' => '1983-01-17',
+ 'Runtime' => '60 mins ',
+ 'ThumbURL' => 'http://images.primewire.ag/thumbs/139442_Frontline_1983.jpg',
+ 'Title' => 'Frontline-US');
+?>
