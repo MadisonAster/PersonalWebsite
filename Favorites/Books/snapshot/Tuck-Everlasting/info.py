8aecc68ff5f22fc33195543126589cc694510846
Madison Aster <info@MadisonAster.com>
2020-06-05 00:30:14 -0700

    testing a third book

---
+++ Favorites/Books/snapshot/Tuck-Everlasting/info.py
@@ -0,0 +1,41 @@
+{'EntryAdded': '06-05-2020',
+ 'EntryPath': 'Favorites/Books/snapshot/Tuck-Everlasting',
+ 'EntryURL': 'https://www.goodreads.com/book/show/84981.Tuck_Everlasting',
+ 'Entry_json': 'Favorites/Books/snapshot/Tuck-Everlasting/entry.json',
+ 'Entry_py': 'Favorites/Books/snapshot/Tuck-Everlasting/info.py',
+ 'Entry_thumb': 'Favorites/Books/snapshot/Tuck-Everlasting/thumb.jpg',
+ 'authors': [{'id': '1954',
+              'image_url': '\n'
+                           'https://images.gr-assets.com/authors/1226361832p5/1954.jpg\n',
+              'link': 'https://www.goodreads.com/author/show/1954.Natalie_Babbitt',
+              'name': 'Natalie Babbitt',
+              'role': None,
+              'small_image_url': '\n'
+                                 'https://images.gr-assets.com/authors/1226361832p2/1954.jpg\n'}],
+ 'description': 'Doomed to - or blessed with - eternal life after drinking '
+                'from a magic spring, the Tuck family wanders about trying to '
+                'live as inconspicuously and comfortably as they can. When '
+                'ten-year-old Winnie Foster stumbles on their secret, the '
+                'Tucks take her home and explain why living forever at one age '
+                'is less a blessing that it might seem. Complications arise '
+                'when Winnie is followed by a stranger who wants to market the '
+                'spring water for a fortune.',
+ 'edition_information': None,
+ 'format': 'Paperback',
+ 'id': '84981',
+ 'image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1445184695l/84981._SX98_.jpg',
+ 'isbn': '0374480095',
+ 'isbn13': '9780374480097',
+ 'large_image_url': None,
+ 'link': 'https://www.goodreads.com/book/show/84981.Tuck_Everlasting',
+ 'num_pages': '148',
+ 'publication_day': '1',
+ 'publication_month': '11',
+ 'publication_year': '1985',
+ 'published': '1985',
+ 'publisher': 'Farrar Straus Giroux',
+ 'small_image_url': 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1445184695l/84981._SY75_.jpg',
+ 'title': 'Tuck Everlasting',
+ 'title_without_series': 'Tuck Everlasting',
+ 'uri': 'kca://book/amzn1.gr.book.v1.SIXDFmigKFUwltQC3Tdxxw',
+ 'work': '  '}
