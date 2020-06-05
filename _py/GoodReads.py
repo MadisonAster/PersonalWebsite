6d293e19cc297e84b2b60da41586e6240659be57
Madison Aster <info@MadisonAster.com>
2020-06-04 23:28:09 -0700

    converting to dict

--- _py/GoodReads.py
+++ _py/GoodReads.py
@@ -12,8 +12,6 @@ import requests
 
 import GatherFavorites
 
-
-
 def example():
     author_id = '1439'
 
@@ -22,7 +20,7 @@ def example():
     response = requests.get('https://www.goodreads.com/author/show.xml?'+'key='+key+'&id='+author_id)
 
     tree = ElementTree.fromstring(response.content)
-
+    
     #print(response.content)
     #print(tree)
 
@@ -55,16 +53,65 @@ def main(user_id, key):
     URLList = GetListData(user_id, 'read', key)
     #URLList = GetListData(user_id, 'favorite', key)
     GatherFavorites.TestData(Entries, URLList)
+
+
+def RemoveReviews(MyDict):
+    delete_keys = ['text_reviews_count', 'ratings_count', 'average_rating']
+    for key in delete_keys:
+        if key in MyDict.keys():
+            del MyDict[key]
+def GetTextData(DataTree):
+    Result = {}
+    for child in DataTree:
+        Result[child.tag] = child.text
+    RemoveReviews(Result)
+    return Result
     
 def GetListData(user_id, shelf, key):
     URLList = []
-    
-    response = requests.get('https://www.goodreads.com/review/list?v=2&id='+user_id+'&shelf='+shelf+'&key='+key)
-    pprint(response.text.split('\n'))
+    page = '1'
+    per_page = '200'
+    response = requests.get('https://www.goodreads.com/review/list?v=2&id='+user_id+'&shelf='+shelf+'&page='+page+'&per_page='+per_page+'&key='+key)
+    #pprint(response.text.split('\n'))
     tree = ElementTree.fromstring(response.content)
+    reviews = tree.find('reviews')
     
-    #for leaves in tree[1]:
-    #    pprint(leaves)
+    Books = []
+    for review in reviews:
+        print(review.tag, review.attrib, review.text)
+        bookdata = review.find('book')
+        Book = GetTextData(bookdata)
+        Authors = []
+        for authordata in bookdata.find('authors'):
+            Author = GetTextData(authordata)
+            Authors.append(Author)
+        Book['authors'] = Authors
+        Books.append(Book)
+    pprint(Books)
+    
+    '''
+    Book['id'] = book.find('id').text
+    Book['isbn'] = book.find('isbn').text
+    Book['isbn13'] = book.find('isbn13').text
+    Book['uri'] = book.find('uri').text
+    Book['title'] = book.find('title').text
+    Book['tite_without_series'] = book.find('tite_without_series').text
+    Book['img_url'] = book.find('img_url').text
+    Book['small_image_url'] = book.find('small_image_url').text
+    Book['large_image_url'] = book.find('large_image_url').text
+    Book['link'] = book.find('link').text
+    Book['num_pages'] = book.find('num_pages').text
+    Book['format'] = book.find('format').text
+    Book['edition_information'] = book.find('edition_information').text
+    Book['publisher'] = book.find('publisher').text
+    Book['publication_day'] = book.find('publication_day').text
+    Book['publication_year'] = book.find('publication_year').text
+    Book['publication_month'] = book.find('publication_month').text
+    Book['description'] = book.find('description').text
+    Book['authors'] = book.find('authors').text
+    Book['published'] = book.find('published').text
+    Book['work'] = book.find('work').text
+    '''
     
     return []
 
