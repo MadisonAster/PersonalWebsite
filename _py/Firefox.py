613257a4d3f00b20973918b4deb421b3522f73d5
Madison Aster <info@MadisonAster.com>
2020-06-06 04:02:42 -0700

    working on Firefox Bookmarks 2

--- _py/Firefox.py
+++ _py/Firefox.py
@@ -22,34 +22,6 @@ def execute_query(cursor, query):
         print(str(error) + "\n " + query)
 
 def get_bookmarks(cursor, TargetFolder='Studies'):
-    #bookmarks_query = """select url, moz_places.title, rev_host, frecency,
-    #last_visit_date from moz_places  join  \
-    #moz_bookmarks on moz_bookmarks.fk=moz_places.id where visit_count>0
-    #and moz_places.url  like 'http%'
-    #order by dateAdded desc;"""
-    #bookmarks_query = "select url, moz_bookmarks.title, from moz_bookmarks;"
-    #bookmarks_query = """
-    #SELECT moz_places.id, moz_places.url, moz_places.title, moz_bookmarks.parent,
-    #moz_bookmarks.dateAdded, moz_bookmarks.folder_type
-    #FROM moz_places
-    #LEFT OUTER JOIN moz_bookmarks
-    #ON moz_places.id = moz_bookmarks.fk
-    #"""
-    #bookmarks_query="""
-    #SELECT moz_bookmarks.title, moz_places.url 
-    #FROM moz_bookmarks 
-    #LEFT JOIN moz_places 
-    #WHERE moz_bookmarks.fk = moz_places.id 
-    #AND moz_bookmarks.title != 'null';
-    #"""
-    #bookmarks_query="""
-    #SELECT moz_bookmarks.title, moz_places.url, moz_bookmarks_roots.root_name
-    #FROM moz_bookmarks AS b
-    #LEFT JOIN moz_places AS p ON b.fk=p.id
-    #LEFT JOIN moz_bookmarks_roots AS r ON b.id=r.folder_id
-    #WHERE b.title != 'null';
-    #"""
-    
     bookmarks_query="""
     SELECT moz_bookmarks.id, moz_bookmarks.title, moz_bookmarks.parent
     FROM moz_bookmarks
