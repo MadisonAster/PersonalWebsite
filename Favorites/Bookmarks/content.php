badc513e0d4001ee00cba2fe6a5bca063eb56338
Madison Aster <info@MadisonAster.com>
2020-06-09 13:35:56 -0700

    Bookmarks tree 32

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -14,7 +14,7 @@ foreach ($Bookmarks['folders'] as &$folder){
     $Bookmarks['description'] = str_replace('\x94', '"', $Bookmarks['description']);
     $Bookmarks['description'] = str_replace('\x97', "", $Bookmarks['description']);
     
-    echo "<li id='item_".folder['id']."' data-module='".folder['id']."'>";
+    echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."'>";
     echo "<div>".$folder['_title']."</div>";
     echo "</li>";
 };
