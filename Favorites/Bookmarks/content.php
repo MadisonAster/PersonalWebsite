99f772bddcede6de235fbdc65940ec642840135d
Madison Aster <info@MadisonAster.com>
2020-06-08 23:27:19 -0700

    Bookmarks tree 7

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -8,22 +8,22 @@ $Entry_json = './Favorites/Bookmarks/snapshot/entry.json'
 $json = file_get_contents($Entry_json);
 $Bookmarks = json_decode($json, TRUE);
 
-foreach ($Bookmarks['folders'] as &$folder){
-    $dirArray = explode("/", $dir);
-    $folderName = current(array_slice($dirArray, -1));
-    $varName = str_replace("-", "", $folderName);
-    
-    $Bookmarks['description'] = str_replace('\n', '', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\r', '', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x92', "'", $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x93', '"', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x94', '"', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x97', "", $Bookmarks['description']);
-    
-    //echo "<li>";
-    //echo "<div>".$Bookmarks['_title']."</div>";
-    //echo "</li>";
-};
+//foreach ($Bookmarks['folders'] as &$folder){
+//    $dirArray = explode("/", $dir);
+//    $folderName = current(array_slice($dirArray, -1));
+//    $varName = str_replace("-", "", $folderName);
+//    
+//    $Bookmarks['description'] = str_replace('\n', '', $Bookmarks['description']);
+//    $Bookmarks['description'] = str_replace('\r', '', $Bookmarks['description']);
+//    $Bookmarks['description'] = str_replace('\x92', "'", $Bookmarks['description']);
+//    $Bookmarks['description'] = str_replace('\x93', '"', $Bookmarks['description']);
+//    $Bookmarks['description'] = str_replace('\x94', '"', $Bookmarks['description'])//;
+//    $Bookmarks['description'] = str_replace('\x97', "", $Bookmarks['description']);
+//    
+//    //echo "<li>";
+//    //echo "<div>".$Bookmarks['_title']."</div>";
+//    //echo "</li>";
+//};
 ?>
 
 </ul>
