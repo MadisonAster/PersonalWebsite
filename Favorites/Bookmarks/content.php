9e51b19bee9dbbeb331bde7b6aa543b46ab12a34
Madison Aster <info@MadisonAster.com>
2020-06-09 00:12:49 -0700

    Bookmarks tree 20

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -1,5 +1,5 @@
 <div id='BookmarksDiv' style='padding:0;margin:0;display:none;text-align:left;'>
-<ul id='BookmarksTree'>
+<ul id='BookmarksTree' class='FolderTree'>
 
 <?php
 $Entry_json = './Favorites/Bookmarks/snapshot/entry.json';
@@ -23,12 +23,45 @@ foreach ($Bookmarks['folders'] as &$folder){
 </ul>
 </div>
 
-<script src='./Favorites/jquery-sortable-lists-mobile.min.js'></script>
+<script type='text/css'>
+#BookmarksTree li, #sortableListsBase li { background-color:#ddf, padding-left:50px }
+</script>
+
+<script src='./Favorites/Bookmarks/jquery-sortable-lists-mobile.js'></script>
 <script>
     console.log('#BookmarksTree');
     console.log($('#BookmarksTree').html());
     var options = {
-        //insertZonePlus: true
+        // Like a css class name. Class will be removed after drop.
+        currElClass: 'currElemClass',
+        // or like a jQuery css object. Note that css object settings can't be removed
+        currElCss: {'background-color':'green', 'color':'#fff'},
+        
+        placeholderClass: 'placeholderClass',
+        // or like a jQuery css object
+        placeholderCss: {'background-color':'yellow'},
+        
+        
+        hintClass: 'hintClass',
+        // or like a jQuery css object
+        hintCss: {'background-color':'green', 'border':'1px dashed white'},
+        
+        listSelector: 'ol',
+        hintWrapperClass: 'hintClass',
+        // or like a jQuery css object
+        hintWrapperCss: {'background-color':'green', 'border':'1px dashed white'},
+    
+        listSelector: 'ol',
+        listsClass: 'FolderTree',
+        // or like a jQuery css object
+        listsCss: {'background-color':'silver', 'border':'1px solid white'},   
+        
+        // All elements with class clickable will be clickable
+        ignoreClass: 'clickable',
+        
+        insertZone: 50,
+        
+        insertZonePlus: true,
     }
     $('#BookmarksTree').sortableLists(options);
 </script>
