34fbcc70ff3dd16154a34ed4038c55aabb35afe4
Madison Aster <info@MadisonAster.com>
2020-06-08 23:32:37 -0700

    Bookmarks tree 9

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -5,7 +5,7 @@
 
 <?php
 $Entry_json = './Favorites/Bookmarks/snapshot/entry.json';
-//$json = file_get_contents($Entry_json);
+$json = file_get_contents($Entry_json);
 //$Bookmarks = json_decode($json, TRUE);
 
 //foreach ($Bookmarks['folders'] as &$folder){
