1580f7b184cbd744585f229be04c040ca1ef8329
Madison Aster <info@MadisonAster.com>
2020-06-09 14:13:32 -0700

    Bookmarks tree 42

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -16,11 +16,11 @@ foreach ($Bookmarks['folders'] as &$folder){
     
     echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."' class='s-l-closed'>";
     echo "<div>".$folder['_title']."</div>";
-    echo "<ul class='' style=''>";
+    echo "<ul class='' style='display:inline-block;'>";
     foreach ($folder['folders'] as &$subfolder){
         echo "<li id='item_".$subfolder['id']."' data-module='".$subfolder['id']."' class='s-l-closed'>";
         echo "<div>".$subfolder['_title']."</div>";
-        echo "<ul class='' style=''>";
+        echo "<ul class='' style='display:inline-block;'>";
         foreach ($subfolder['links'] as &$link){
             echo "<li id='item_".$link['id']."' data-module='".$link['id']."' class='s-l-closed'>";
             echo "<div><a target='_blank' href='".$link['url']."'>".$link['_title']."</a></div>";
