1bc874833f349208f27ecdffb47899ddbb7e2d3a
Madison Aster <info@MadisonAster.com>
2020-06-09 00:43:56 -0700

    Bookmarks tree 26

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -1,3 +1,27 @@
+<div id='BookmarksDiv' style='padding:0;margin:0;display:none;text-align:left;'>
+<ul id='BookmarksTree listsClass' class='BookmarksTree'>
+
+<?php
+$Entry_json = './Favorites/Bookmarks/snapshot/entry.json';
+$json = file_get_contents($Entry_json);
+$Bookmarks = json_decode($json, TRUE);
+
+foreach ($Bookmarks['folders'] as &$folder){
+    $Bookmarks['description'] = str_replace('\n', '', $Bookmarks['description']);
+    $Bookmarks['description'] = str_replace('\r', '', $Bookmarks['description']);
+    $Bookmarks['description'] = str_replace('\x92', "'", $Bookmarks['description']);
+    $Bookmarks['description'] = str_replace('\x93', '"', $Bookmarks['description']);
+    $Bookmarks['description'] = str_replace('\x94', '"', $Bookmarks['description']);
+    $Bookmarks['description'] = str_replace('\x97', "", $Bookmarks['description']);
+    
+    echo "<li id='item_".folder['id']."' data-module='".folder['id']."'>";
+    echo "<div>".$folder['_title']."</div>";
+    echo "</li>";
+};
+?>
+
+</ul>
+</div>
 
 <script type='text/css'>
 #BookmarksTree li, #sortableListsBase li { background-color:#ddf, padding-left:50px }
@@ -136,29 +160,3 @@
     $('#BookmarksTree').sortableLists(options);
     
 </script>
-
-<div id='BookmarksDiv' style='padding:0;margin:0;display:none;text-align:left;'>
-<ul id='BookmarksTree listsClass' class='BookmarksTree'>
-
-<?php
-$Entry_json = './Favorites/Bookmarks/snapshot/entry.json';
-$json = file_get_contents($Entry_json);
-$Bookmarks = json_decode($json, TRUE);
-
-foreach ($Bookmarks['folders'] as &$folder){
-    $Bookmarks['description'] = str_replace('\n', '', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\r', '', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x92', "'", $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x93', '"', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x94', '"', $Bookmarks['description']);
-    $Bookmarks['description'] = str_replace('\x97', "", $Bookmarks['description']);
-    
-    echo "<li id='item_".folder['id']."' data-module='".folder['id']."'>";
-    echo "<div>".$folder['_title']."</div>";
-    echo "</li>";
-};
-?>
-
-</ul>
-</div>
-
