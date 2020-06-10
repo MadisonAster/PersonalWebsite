144416b876d26808f7a25fe645c800775eab6305
Madison Aster <info@MadisonAster.com>
2020-06-09 20:29:47 -0700

    Bookmarks tree 79

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -27,8 +27,7 @@ function RecursivelyPrintFolders($folders) {
         echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."' class='s-l-closed clickable'>";
         if (strpos($folder['_title'], '|') !== false) {
             list($none, $title, $year, $level) = split('|', $folder['_title'], 4);
-            
-            echo "<div class='clickable'>".$title."</div>";
+            echo "<div class='clickable'>".$year."</div>";
         } else {
             echo "<div class='clickable'>".$folder['_title']."</div>";
         };
