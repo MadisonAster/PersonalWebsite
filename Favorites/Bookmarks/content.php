a5299cd5cb4bf697654ce2293aea9b3f59f177a9
Madison Aster <info@MadisonAster.com>
2020-06-10 01:48:07 -0700

    tree styling 24

--- Favorites/Bookmarks/content.php
+++ Favorites/Bookmarks/content.php
@@ -25,23 +25,30 @@ function SortByPosition($a, $b) {
     return $a['position'] - $b['position'];
 }
 
-function RecursivelyPrintFolders($folders) {
+function RecursivelyPrintFolders($folders, $colors, $root=false, $color=null) {
     usort($folders, 'SortByPosition');
     foreach ($folders as &$folder){
+        if ($root){
+            $colorindex = intval($folder['position']) % sizeof($colors);
+            $color = $colors[$colorindex];
+        }
         if(substr($folder['_title'], 0, 1) !== '_'){
             if (strpos($folder['_title'], '|') !== false) {
-                
-            echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."' class='sortableListsClosed clickable'>";
+                echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."' class='sortableListsClosed clickable'>";
                 list($none, $title, $year, $level) = explode('|', $folder['_title']);
                 echo "<div class='clickable'>";
                     echo $title."<span class='clickable' style='color:#4f4f4f;'>  -  </span><span class='clickable' style='color:rgba(0,255,0,0.".substr($level,0,2).");'>".$level."</span><span class='clickable' style='color:#4f4f4f;'> Proficiency    |    First Used ".$year."</span>";
                 echo "</div>";
             } else {
-                echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."' class='sortableListsOpen clickable'>";
+                if ($root){
+                    echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."' class='sortableListsOpen clickable' style='border-color:#".$color.";background-color:#".$color.";'>";
+                } else {
+                    echo "<li id='item_".$folder['id']."' data-module='".$folder['id']."' class='sortableListsOpen clickable'>";
+                }
                 echo "<div class='clickable'>".$folder['_title']."</div>";
             };
             echo "<ol class='clickable'>";
-            RecursivelyPrintFolders($folder['folders']);
+            RecursivelyPrintFolders($folder['folders'], $colors, false, $color);
             $links = $folder['links'];
             usort($links, 'SortByPosition');
             foreach ($folder['links'] as &$link){
@@ -52,8 +59,15 @@ function RecursivelyPrintFolders($folders) {
         };
     };
 };
-
-RecursivelyPrintFolders($Bookmarks['folders']);
+$Colors = Array(
+    'D23747',
+    'DAA35B',
+    'DDDD6B',
+    '1A742B',
+    '3333D1',
+    '781C72',
+);
+RecursivelyPrintFolders($Bookmarks['folders'], $Colors, true, null);
 ?>
 
 </ol>
