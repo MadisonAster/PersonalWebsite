f7c076a986c40beabddabb0bfe64f18b2653d670
Madison Aster <info@MadisonAster.com>
2020-06-13 00:34:17 -0700

    resume dialog 46

--- About/PDFGeneration.js
+++ About/PDFGeneration.js
@@ -12,7 +12,7 @@ function ResumeDialog() {
     });
     for (var i = 0; i < window.Skills.length; i++) {
         var Skill = window.Skills[i];
-        var title = Skill['title'].split('>',1).slice(-1)[0].split('<',1)[0];
+        var title = Skill['title'].split('>',1).slice(-1)[0];
         console.log(title);
         $('#DialogSkillsCloud').append('<button type="button" class="SkillButton">'+title+'</button>');
     };
