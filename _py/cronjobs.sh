8431ffd06a6da708bbe2493602862bb5f66ee284
Madison Aster <info@MadisonAster.com>
2020-06-18 19:49:27 -0700

    adding default cronjobs.sh

---
+++ _py/cronjobs.sh
@@ -0,0 +1,8 @@
+python3 /var/www/html/_py/GoodReads.py {USER_ID_NUMBER} {API_KEY}
+python3 /var/www/html/_py/Rawg.py {PROFILE_NAME}
+python3 /var/www/html/_py/TakeSnapshots.py GetGitHub {GITHUB_URL} GitHub/snapshot
+python3 /var/www/html/_py/TakeSnapshots.py GetIMDB {IMDB_URL} IMDB/snapshot
+python3 /var/www/html/_py/TakeSnapshots.py GetCodeWars {CODEWARS_URL} CodeWars/snapshot
+python3 /var/www/html/_py/TakeSnapshots.py GetLinkedIn {LINKEDIN_URL} GetLinkedIn/snapshot
+python3 /var/www/html/_py/IMDB.py {FAVORITE_TV_URL} Favorites/TV/snapshot/
+python3 /var/www/html/_py/IMDB.py {FAVORITE_MOVIES_URL} Favorites/Movies/snapshot/
