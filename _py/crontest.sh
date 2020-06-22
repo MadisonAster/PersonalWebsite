8122ca01755964a1d29d41d9360a54f53ff069c5
Madison Aster <info@MadisonAster.com>
2020-06-22 01:04:25 -0700

    correcting cron job line endings

--- _py/crontest.sh
+++ _py/crontest.sh
@@ -1,2 +1,2 @@
 * * * * * echo "Hello world" >> /var/log/cron.log 2>&1
-# An empty line is required at the end of this file for a valid cron file.
+# An empty line is required at the end of this file, line endings must be unix style (LF Only)
