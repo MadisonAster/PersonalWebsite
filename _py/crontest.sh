639af1a1ce7eaf44571601609f4a7ba33b9892b8
Madison Aster <info@MadisonAster.com>
2020-06-22 02:16:13 -0700

    installing python packages

--- _py/crontest.sh
+++ _py/crontest.sh
@@ -1,2 +1,10 @@
-* * * * * echo "Hello world" >> /var/log/cron.log 2>&1
+* * * * * echo "Hello world" >> /cron.log
+* * * * * echo "/var/www/html/_py/GoodReads.py" >> /cron_GoodReads.log
+* * * * * echo "/var/www/html/_py/Rawg.py" >> /cron_Rawg.log
+* * * * * echo "/var/www/html/_py/TakeSnapshots.py" >> /cron_GitHub.log
+* * * * * echo "/var/www/html/_py/TakeSnapshots.py" >> /cron_IMDB.log
+* * * * * echo "/var/www/html/_py/TakeSnapshots.py" >> /cron_CodeWars.log
+* * * * * echo "/var/www/html/_py/TakeSnapshots.py" >> /cron_LinkedIn.log
+* * * * * echo "/var/www/html/_py/IMDB.py" >> /cron_TV.log
+* * * * * echo "/var/www/html/_py/IMDB.py" >> /cron_Movies.log
 # An empty line is required at the end of this file, line endings must be unix style (LF Only)
