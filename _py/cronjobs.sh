4101a4b2af3ee36b0b5f326050d337a45d5de6a6
Madison Aster <info@MadisonAster.com>
2020-06-24 01:16:35 -0700

    updating cronjobs.sh

--- _py/cronjobs.sh
+++ _py/cronjobs.sh
@@ -1,8 +1,9 @@
-python3 /var/www/html/_py/GoodReads.py {USER_ID_NUMBER} {API_KEY}
-python3 /var/www/html/_py/Rawg.py {PROFILE_NAME}
-python3 /var/www/html/_py/TakeSnapshots.py GetGitHub {GITHUB_URL} GitHub/snapshot
-python3 /var/www/html/_py/TakeSnapshots.py GetIMDB {IMDB_URL} IMDB/snapshot
-python3 /var/www/html/_py/TakeSnapshots.py GetCodeWars {CODEWARS_URL} CodeWars/snapshot
-python3 /var/www/html/_py/TakeSnapshots.py GetLinkedIn {LINKEDIN_URL} GetLinkedIn/snapshot
-python3 /var/www/html/_py/IMDB.py {FAVORITE_TV_URL} Favorites/TV/snapshot/
-python3 /var/www/html/_py/IMDB.py {FAVORITE_MOVIES_URL} Favorites/Movies/snapshot/
+00 00 * * * python3 /_py/GoodReads.py {GoodReads_UserNumber} {GoodReads_ApiKey} >> /cron_GoodReads.log
+10 00 * * * python3 /_py/Rawg.py {Rawg_UserName} >> /cron_Rawg.log
+20 00 * * * python3 /_py/TakeSnapshots.py GetGitHub {GitHub_SnapshotURL} GitHub/snapshot >> /cron_GitHub.log
+30 00 * * * python3 /_py/TakeSnapshots.py GetIMDB {IMDB_SnapshotURL} IMDB/snapshot >> /cron_IMDB.log
+30 00 * * * python3 /_py/TakeSnapshots.py GetCodeWars {CodeWars_SnapshotURL} CodeWars/snapshot >> /cron_CodeWars.log
+30 00 * * * python3 /_py/TakeSnapshots.py GetLinkedIn {LinkedIn_SnapshotURL} GetLinkedIn/snapshot >> /cron_LinkedIn.log
+30 00 * * * python3 /_py/IMDB.py {FavoriteTV_ListURL} Favorites/TV/snapshot/ >> /cron_TV.log
+30 00 * * * python3 /_py/IMDB.py {FavoriteMovies_ListURL} Favorites/Movies/snapshot/ >> /cron_Movies.log
+# An empty line is required at the end of this file, line endings must be unix style (LF Only)
