15af097f9832603d8684502a23e9c3d9828f7dde
Madison Aster <info@MadisonAster.com>
2020-07-24 13:33:05 -0700

    ecr push

--- _setup/ecr_push.sh
+++ _setup/ecr_push.sh
@@ -5,7 +5,12 @@ eval $(terraform output | sed 's/^/export /; s/ = /="/g; s/$/"/')
 cd ../../_setup
 
 export datascraper_image_id=$(docker images datascraper:latest --format "{{.ID}}")
+export myapache_image_id=$(docker images myapache:latest --format "{{.ID}}")
 
 aws ecr get-login-password --region $aws_region | docker login --username AWS --password-stdin $aws_user_id.dkr.ecr.$aws_region.amazonaws.com
+
 docker tag $datascraper_image_id $aws_user_id.dkr.ecr.$aws_region.amazonaws.com/rpp_datascraper-service
 docker push $aws_user_id.dkr.ecr.$aws_region.amazonaws.com/rpp_datascraper-service
+
+docker tag $myapache_image_id $aws_user_id.dkr.ecr.$aws_region.amazonaws.com/rpp_myapache-service
+docker push $aws_user_id.dkr.ecr.$aws_region.amazonaws.com/rpp_myapache-service
