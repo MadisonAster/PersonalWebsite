e7f59e88dec6ae6d9e7484ec81b981aae939b837
Madison Aster <info@MadisonAster.com>
2020-07-31 09:56:36 -0700

    adding ExtractTags function

---
+++ About/ExtractTags.py
@@ -0,0 +1,37 @@
+import os, sys
+import csv
+from pprint import pprint
+
+def main():
+    wd = os.path.dirname(os.path.abspath(__file__))
+    WordListPath = wd+'/WordList.csv'
+    print(WordListPath)
+    CurrentList = []
+    with open(WordListPath, 'r') as file:
+        csvreader = csv.reader(file, delimiter=',')
+        for value in csvreader:
+            for item in value:
+                if item not in ['', '\n', '\r', ' ', '\t']:
+                    CurrentList.append(item)
+
+    ScanTextPath = wd+'/WordScan.txt'
+    NewList = []
+    with open(ScanTextPath, 'r') as file:
+        filetext = file.read();
+        for word in filetext.split(' '):
+            santizedword = ''
+            for letter in word:
+                if letter.isalpha():
+                    santizedword += letter
+            if santizedword not in ['', '\n', '\r', ' ', '\t']:
+                NewList.append(santizedword)
+
+    CombinedList = list(set(CurrentList + NewList))
+    newcount = len(CombinedList) - len(CurrentList)
+    print(newcount, 'new words!')
+    with open(WordListPath, 'w') as file:
+        for item in CombinedList:
+            file.write(item+',\n')
+    
+if __name__ == '__main__':
+    main()
